0a1dbe28b725e328d820cf6ea59e471713ac8f6c
WillhardtR57 <willhardt21001@student.hamk.fi>
2022-02-18 09:31:49 +0200

    shit

--- php/config.php
+++ php/config.php
@@ -3,12 +3,14 @@
 define('DB_SERVER', 'db');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'password');
-define('DB_NAME', 'demo');
+define('DB_NAME1', 'demo');
  
 
-$link = mysqli_connect(DB_SERVER , DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER , DB_USERNAME, DB_PASSWORD, DB_NAME1);
  
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+
 ?>
